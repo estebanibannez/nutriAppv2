613e9a01602f368cae93a37a199c3a864847ef5b
Esteban Ibañez <e.ibannez.p@gmail.com>
2021-03-07 17:22:14 -0300

    port

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const options = {
 		servers: [
 			{
 				// url: "http://localhost:3000",
-				url: process.env.PATHSWAGGER || "http://localhost:3000",
+				url: process.env.PATHSWAGGER || "http://localhost:8080",
 			},
 		],
 	},
